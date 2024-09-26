1033c80c22981aeb49496852eddcd426cbfb3df1
Caleb <justcaleb01@gmail.com>
2024-09-26 09:06:58 +0100

    "Updated CSS class name and added image preview functionality to ProductDetails component"

--- src/pages/productDetails.jsx
+++ src/pages/productDetails.jsx
@@ -1,6 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import UseFetch from "../components/hooks/useFetch";
 import Navbar from "../components/navbar";
+import { useState } from "react";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -9,6 +10,8 @@ const ProductDetails = () => {
     loading,
     data: product,
   } = UseFetch("https://dummyjson.com/products/" + id);
+
+  const [previewIndex, setPreviewIndex] = useState(0);
 
   return (
     <div>
@@ -29,34 +32,50 @@ const ProductDetails = () => {
         {loading && <p>Loading...</p>}
 
         {/* Make reusable component here */}
-          {!loading && !err && (
-            <div className="flex gap-[70px] items-start justify-center">
-              {/* images */}
-              <div className="grid-container">
-                {
-                  product.images.map((_,i)=>{
-                    return <div><img src={product.images[i]} alt={product.title} key={i} className=""/></div>
-                  })
-                }
-                <div className="cols"><img src={product.images[0]} alt="" className="object-contain w-full h-full"/></div>
-              </div>
-
-              {/* details */}
-              <div className="flex flex-col gap-6">
-                <div className="flex flex-col gap-4">
-                  <h2 className="font-semibold text-[24px]">{product.title}</h2>
-                  <div className="flex items-center gap-2">
-                    <div>rating...</div>
-                    <div className="text-[14px]">({product.reviews.length} Reviews)</div>
-                    <div className="text-[#00FF66] text-[14px] border-l border-black px-2">In Stock</div>
-                  </div>
-                  <h2 className="text-[24px]">${product.price}</h2>
-                </div>
-                <p>{product.description}</p>
-                <hr className="text-black"/>
-              </div>
+        {!loading && !err && (
+          <div className="flex gap-[70px] items-start">
+            {/* images */}
+            <div className="grid-container">
+              {product.images.map((_, i) => {
+                return (
+                  <img
+                    src={product.images[i]}
+                    onClick={() => {
+                      setPreviewIndex(i);
+                    }}
+                    alt={product.title}
+                    key={i}
+                    className="object-contain w-[full] bg-[#F5F5F5] h-[140px] cursor-pointer rounded"
+                  />
+                );
+              })}
+              <img
+                src={product.images[previewIndex]}
+                alt=""
+                className="object-contain w-full h-full bg-[#F5F5F5] rounded preview"
+              />
             </div>
-          )}
+
+            {/* details */}
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-4">
+                <h2 className="font-semibold text-[24px]">{product.title}</h2>
+                <div className="flex items-center gap-2">
+                  <div>rating...</div>
+                  <div className="text-[14px]">
+                    ({product.reviews.length} Reviews)
+                  </div>
+                  <div className="text-[#00FF66] text-[14px] border-l border-black px-2">
+                    In Stock
+                  </div>
+                </div>
+                <h2 className="text-[24px]">${product.price}</h2>
+              </div>
+              <p>{product.description}</p>
+              <hr className="text-black" />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
